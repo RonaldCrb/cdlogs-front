77b67020a73d6235984bd10f96217096e114be88
Ron Alonzo <alonzo.ronald@gmail.com>
2019-09-08 23:15:45 +0200

    hotfix

--- pages/planner.vue
+++ pages/planner.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
     <Banner />
-    <DivePlanner />
   </div>
 </template>
 
 <script>
 import Banner from '@/components/Home/Banner.vue'
-import DivePlanner from '@/components/Home/DivePlanner'
 export default {
   components: {
-    DivePlanner,
     Banner
   }
 }
